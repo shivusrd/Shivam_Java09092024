310c84f325fc70c3b70442560b19e62b0f26f63a
shivam dubey <95178183+shivusrd@users.noreply.github.com>
2024-09-09 15:53:45 +0530

    Update ArmstrongPractice.java

--- src/com/shivam/arrayprograms/ArmstrongPractice.java
+++ src/com/shivam/arrayprograms/ArmstrongPractice.java
@@ -2,34 +2,60 @@ package com.shivam.arrayprograms;
 
 import java.util.Scanner;
 
-public class ArmstrongPractice 
-{
-	    public static void main(String[] args) {
-	        Scanner scanner = new Scanner(System.in);
-	        System.out.print("Enter a number: ");
-	        int number = scanner.nextInt();
-	        scanner.close();
-	        
-	        if (isArmstrong(number)) {
-	            System.out.println(number + " is an Armstrong number.");
-	        } else {
-	            System.out.println(number + " is not an Armstrong number.");
-	        }
-	    }
+public class ArmstrongPractice {
 
-	    public static boolean isArmstrong(int number) {
-	        int originalNumber = number;
-	        int sum = 0;
-	        int digits = String.valueOf(number).length(); // Number of digits in the number
+	public static void main(String[] args) {
 
-	        while (number != 0) {
-	            int digit = number % 10; // Extract the last digit
-	            sum += Math.pow(digit, digits); // Add the digit raised to the power of the number of digits
-	            number /= 10; // Remove the last digit
-	        }
+		Scanner sc = new Scanner(System.in);
+		System.out.println("please enter a number");
+		int number = sc.nextInt();
+		sc.close();
 
-	        return sum == originalNumber;
-	    }
+		int originalNum = number;
+		int sum = 0;
+		int length = countDigits(number);// we can also use countDigits method
+
+		while (number != 0) {
+			int digit = number % 10;
+			sum += power(digit, length);
+			//sum += Math.pow(digit, digits); // We can also use power method manually
+			number /= 10;
+
+		}
+
+		if (sum == originalNum)
+
+		{
+
+			System.out.println(originalNum + " is Armstrong");
+		}
+
+		else {
+
+			System.out.println(originalNum + " is not Armstrong");
+		}
+
 	}
+
+	// Method to calculate base^exponent manually
+	public static int power(int base, int exponent) {
+		int result = 1;
+		for (int i = 0; i < exponent; i++) {
+			result *= base; // Multiply base exponent times
+		}
+		return result;
+	}
+
+	// Method to count the number of digits in a number
+	public static int countDigits(int number) {
+		int count = 0;
+		while (number != 0) {
+			count++;
+			number /= 10;
+		}
+		return count;
+	}
+
+}
 
 
